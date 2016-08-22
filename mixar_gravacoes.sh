ccd13c6f91d394c8383e7178ad187e3b261b83c7
Ivan de Almeida Cornedi <iacornedi@gmail.com>
2016-08-22 14:15:41 -0300

    mixar_gravacoes.sh
    Solução paliativa para Elastix com Asternic (não me pergunte porque isso existia) que não mixava as gravações

---
+++ mixar_gravacoes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#mixar_gravacoes.sh
+#
+#	Script utilizado para mixar gravações da ponta A e ponta B.
+#  Desenvolvi este scrip como solução paleativa para um problema que ocorria por causa do Elastix+Asternic.
+#  A pessoa que implantou o asterisk não sabia por que ocorria este problema e não tive oportunidade  
+#de investigar a fundo sua causa. Como o cliente precisava das gravações, criei este script e deixei
+#agendado no cron para executar ao fim do expediente.
+#
+
+#Lista as gravações que não foram mixadas e atriboe a ${NOMEIN} e ${NOMEOUT}
+NOMEIN=($(ls /var/spool/asterisk/monitor/q*-in.WAV))
+NOMEOUT=($(ls /var/spool/asterisk/monitor/q*-out.WAV))
+
+#--- Mixar
+for ((i=0;$i<=${#NOMEIN[@]};i++))
+ do
+	#--- Usado para teste - Caso queira o que ocorrerá  
+	#--- echo " in -> ${NOMEIN[$i]}  out -> ${NOMEOUT[$i]} nome ->  $(ls ${NOMEIN[$i]} | cut -c 1-100 | cut -d. -f 1,2 | cut -d- -f1-4).WAV"
+	#--- 
+	sox -m ${NOMEIN[$i]} ${NOMEOUT[$i]} $(ls ${NOMEIN[$i]} | cut -c 1-100 | cut -d. -f 1,2 | cut -d- -f1-4).WAV
+ done
+
+#Apagar arquivos não mixados
+rm -rfv /var/spool/asterisk/monitor/q*in.WAV
+rm -rfv /var/spool/asterisk/monitor/q*out.WAV
+
+
