ce005a338a5667768b3f5bc4668cb5d196122764
Ivan de Almeida Cornedi <iacornedi@gmail.com>
2016-08-25 07:44:38 -0300

    Update mixar_gravacoes.sh

--- mixar_gravacoes.sh
+++ mixar_gravacoes.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #mixar_gravacoes.sh
 #
-#	Script utilizado para mixar gravaÁıes da ponta A e ponta B.
-#  Desenvolvi este scrip como soluÁ„o paleativa para um problema que ocorria por causa do Elastix+Asternic.
-#  A pessoa que implantou o asterisk n„o sabia por que ocorria este problema e n„o tive oportunidade  
-#de investigar a fundo sua causa. Como o cliente precisava das gravaÁıes, criei este script e deixei
+#	Script utilizado para mixar grava√ß√µes da ponta A e ponta B.
+#  Desenvolvi este script como solu√ß√£o paliativa para um problema que ocorria por causa do Elastix+Asternic.
+#  A pessoa que implantou o asterisk n√£o sabia por que ocorria este problema e eu n√£o tive oportunidade  
+#de investigar a fundo sua causa. Como o cliente precisava das grava√ß√µes, criei este script e deixei
 #agendado no cron para executar ao fim do expediente.
 #
 
-#Lista as gravaÁıes que n„o foram mixadas e atriboe a ${NOMEIN} e ${NOMEOUT}
+#Lista as grava√ß√µes que n√£o foram mixadas e atriboe a ${NOMEIN} e ${NOMEOUT}
 NOMEIN=($(ls /var/spool/asterisk/monitor/q*-in.WAV))
 NOMEOUT=($(ls /var/spool/asterisk/monitor/q*-out.WAV))
 
 #--- Mixar
 for ((i=0;$i<=${#NOMEIN[@]};i++))
  do
-	#--- Usado para teste - Caso queira o que ocorrer·  
+	#--- Usado para teste - Caso queira o que ocorrer√°  
 	#--- echo " in -> ${NOMEIN[$i]}  out -> ${NOMEOUT[$i]} nome ->  $(ls ${NOMEIN[$i]} | cut -c 1-100 | cut -d. -f 1,2 | cut -d- -f1-4).WAV"
 	#--- 
 	sox -m ${NOMEIN[$i]} ${NOMEOUT[$i]} $(ls ${NOMEIN[$i]} | cut -c 1-100 | cut -d. -f 1,2 | cut -d- -f1-4).WAV
  done
 
-#Apagar arquivos n„o mixados
+#Apagar arquivos n√£o mixados
 rm -rfv /var/spool/asterisk/monitor/q*in.WAV
 rm -rfv /var/spool/asterisk/monitor/q*out.WAV
 
